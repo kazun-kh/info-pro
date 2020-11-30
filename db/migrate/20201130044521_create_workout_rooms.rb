3678e5f911c2d644c7e90e8f6eccdf41a625ab03
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-11-30 13:52:26 +0900

    workout_roomsテーブルの作成

--- db/migrate/20201130044521_create_workout_rooms.rb
+++ db/migrate/20201130044521_create_workout_rooms.rb
@@ -1,7 +1,7 @@
 class CreateWorkoutRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :workout_rooms do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
