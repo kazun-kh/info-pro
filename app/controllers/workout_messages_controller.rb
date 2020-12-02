d360165734b080caa0e6296f95e948c3e3f025f8
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-02 15:07:02 +0900

    メッセージの表示

--- app/controllers/workout_messages_controller.rb
+++ app/controllers/workout_messages_controller.rb
@@ -3,22 +3,23 @@ class WorkoutMessagesController < ApplicationController
   def index
     @workout_message = WorkoutMessage.new
     @workout_room = WorkoutRoom.find(params[:workout_room_id])
-   
+    @workout_messages = @workout_room.workout_messages.includes(:user)
   end
 
   def create
     @room = WorkoutRoom.find(params[:workout_room_id])
     @message = @room.workout_messages.new(message_params)
     if @message.save
-      redirect_to  workout_room_workout_messages_path(@room)
+    redirect_to  workout_room_workout_messages_path(@room)
     else
-      render :index
+    @workout_messages = @workout_room.workout_messages.includes(:user)
+    render :index
     end 
   end
 
   private
 
   def message_params
-    params.require(:workout_message).permit(:content).merge(user_id: current_user.id)
+   params.require(:workout_message).permit(:content).merge(user_id: current_user.id)
   end
 end
