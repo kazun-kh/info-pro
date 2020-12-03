efb29c17f51f29349f6c93526c289752786618d6
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-03 11:09:56 +0900

    降順の設定

--- app/controllers/work_outs_controller.rb
+++ app/controllers/work_outs_controller.rb
@@ -2,7 +2,7 @@ class WorkOutsController < ApplicationController
   before_action :authenticate_user!, only: :index
   
   def index
-  @workoutroom = WorkoutRoom.all
+  @workoutroom = WorkoutRoom.all.order(id: "DESC")
   
   
   end
