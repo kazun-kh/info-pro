65ed41225310c5cf583f48551d5dc8e8d954c31c
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-05 19:30:34 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,12 +10,8 @@ require("@rails/activestorage").start()
 require("channels");
 require("../colc_protein");
 // require("../memo");
-<<<<<<< Updated upstream
 
 require('./preview')
-=======
-require('./preview');
->>>>>>> Stashed changes
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
