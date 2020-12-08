7c89f0fc59f8a09bdaaf47e3131f2b90e3614bb4
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-08 18:05:15 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels");
 require("../colc_protein");
+// require("../memo");
 require('./preview')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
