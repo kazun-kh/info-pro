ab48c0b7e31420b3a69bd3ad9fd4c812cf2a4606
Kazuki Hoshiyama <kxxxkhkh@gmail.com>
2020-12-17 21:01:05 +0900

    メッセージの削除（仮）

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :makers 
     resources :work_outs, only: :index 
     resources :workout_rooms, only: [:new, :create, :destroy] do
-      resources :workout_messages, only: [:index, :create]
+      resources :workout_messages, only: [:index, :create, :destroy]
     end
   
 end
